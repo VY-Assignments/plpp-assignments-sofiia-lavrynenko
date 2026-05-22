b943d496271b0be128c8188e9d5018052055f67c
Sofiia Lavrynenko <sofiia.lavrynenko.25@kse.org.ua>
2026-05-22 18:22:52 +0300

    feat: implement text memory storage (via linked list)

--- TextEditor/main.c
+++ TextEditor/main.c
@@ -2,9 +2,65 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+struct Node
+{
+    char* letters;
+    int currLength;
+    int capacity;
+    struct Node* next;
+};
+
+struct Node* CreateNewLine()
+{
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    if (!newNode)
+    {
+        printf("There is a problem. Cannot allocate memory for this node. \n");
+        printf("\n");
+
+        exit(1);
+    }
+
+    newNode -> capacity = 20;
+
+    newNode -> letters = (char*)malloc(sizeof(char) * newNode -> capacity);
+
+    if(!newNode -> letters)
+    {
+        printf("There is a problem. Cannot allocate memory for this char array. \n");
+        printf("\n");
+
+        exit(1);
+    }
+
+    newNode -> letters[0] = '\0';
+
+    newNode -> currLength = 0;
+    newNode -> next = NULL;
+
+    return newNode;
+}
+
+void FreeAllList(struct Node* head)
+{
+    struct Node* current = head;
+
+    while (current != NULL)
+    {
+        struct Node* next = current -> next;
+        free(current -> letters);
+        free(current);
+        current = next;
+    }
+}
+
 int main()
 {
     bool isRunning = true;
+
+    struct Node* head = CreateNewLine();
+    struct Node* current = head;
 
     while (isRunning)
     {
@@ -81,6 +137,7 @@ int main()
             {
                 printf("Exiting the program. \n");
                 printf("\n");
+                FreeAllList(head);
                 isRunning = false;
                 break;
             }
